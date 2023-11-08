a49cbfa8064e27036ca26661eea0d4d405ae2bd1
Jephthah <jephthahndukwe@gmail.com>
2023-11-08 17:06:52 +0100

    new update

--- controllers/Product.js
+++ controllers/Product.js
@@ -1,18 +1,18 @@
 import ProductModel from "../models/ProductModel.js"
 
 //Create new product (Admin) => /api/v1/product/admin/product/new
-export const createProductController = async(req,res) => {
+export const createProductController = async (req, res) => {
     const product = await ProductModel.create(req.body)
-    
+
     try {
-        if(product){
+        if (product) {
             res.status(200).send({
                 success: true,
-                message:"Product created successfully",
+                message: "Product created successfully",
                 product
             })
         }
-    }  catch (error) {
+    } catch (error) {
         console.log(error)
         res.status(500).send({
             success: false,
@@ -23,16 +23,16 @@ export const createProductController = async(req,res) => {
 }
 
 //Get all product => /api/v1/product/products
-export const getProductController = async(req, res) => {
+export const getProductController = async (req, res) => {
     const products = await ProductModel.find({})
     const productsCount = await ProductModel.countDocuments();
-    
+
     try {
-        if(products){
+        if (products) {
             setTimeout(() => {
                 res.json({
                     success: true,
-                    message:"All products retrieved",
+                    message: "All products retrieved",
                     productsCount,
                     products
                 })
@@ -42,15 +42,37 @@ export const getProductController = async(req, res) => {
         console.log(error)
         res.status(500).send({
             success: false,
-            message:"Product does not exist",
+            message: "Product does not exist",
             error
         })
     }
 }
 
+// search product => /api/v1/serach
+export const searchProductController = async (req, res) => {
+    try {
+        const { keyword } = req.params
+        const results = await ProductModel.find({
+            $or: [
+                { name: { $regex: keyword, $options: "i" } },
+                { description: { $regex: keyword, $options: "i" } },
+            ],
+        }).select("-photo");
+        res.json(results);
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({
+            success: false,
+            message: 'Error In Search Product API',
+            error
+        })
+    }
+}
+
+
 //Get all product (Admin) => /api/v1/product/admin/products
 export const getAdminProductsController = async (req, res, next) => {
-        
+
     const products = await ProductModel.find();
 
     setTimeout(() => {
@@ -62,16 +84,18 @@ export const getAdminProductsController = async (req, res, next) => {
 }
 
 //Get single product details   => /api/v1/product/product/:id
-export const getSingleProductController = async(req, res, next) => {
-    const product = await ProductModel.findById(req.params.id);
+export const getSingleProductController = async (req, res, next) => {
+    const product = await ProductModel.findOne({ slug: req.params.slug })
+    
     try {
         res.status(200).json({
             success: true,
+            message: "Single product fetched",
             product
         })
     } catch (error) {
         console.log(error)
-        if(!product) {
+        if (!product) {
             res.status(404).send({
                 success: false,
                 message: 'Product not found',
@@ -82,7 +106,7 @@ export const getSingleProductController = async(req, res, next) => {
 }
 
 //Update Product (Admin)  => /api/v1/product/admin/product/:id
-export const updateProductController = async(req, res, next) => {
+export const updateProductController = async (req, res, next) => {
     let product = await ProductModel.findById(req.params.id);
 
     try {
@@ -91,7 +115,7 @@ export const updateProductController = async(req, res, next) => {
             runValidators: true,
             useFindAndModify: false,
         })
-    
+
         res.status(200).json({
             success: true,
             message: "Product updated successfully",
@@ -111,7 +135,7 @@ export const updateProductController = async(req, res, next) => {
 }
 
 //Delete Product (Admin)  => /api/v1/product/admin/product/:id
-export const deleteProductController = async(req, res, next) => {
+export const deleteProductController = async (req, res, next) => {
     const product = await ProductModel.findByIdAndDelete(req.params.id);
 
     try {
--- route/product.js
+++ route/product.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createProductController, deleteProductController, getAdminProductsController, getProductController, getSingleProductController, updateProductController } from '../controllers/Product.js'
+import { createProductController, deleteProductController, getAdminProductsController, getProductController, getSingleProductController, searchProductController, updateProductController } from '../controllers/Product.js'
 import { isAdmin, requireSignIn } from '../middleware/authMiddleware.js';
 
 
@@ -12,19 +12,14 @@ router.get('/products', getProductController);
 
 router.get('/admin/products', requireSignIn, isAdmin, getAdminProductsController);
 
-router.get('/product/:id', getSingleProductController);
+router.get('/product/:slug', getSingleProductController);
 
 router.put('/admin/product/:id', requireSignIn, isAdmin, updateProductController);
 
 router.delete('/admin/product/:id', requireSignIn, isAdmin, deleteProductController);
 
-
-
-
-
-
-
-
+// search product
+router.get('/search/:keyword', searchProductController);
 
 
 
